b1703d6cc0787a942b566db064abeadc1181976d
ainz <ainz713@gmail.com>
2021-10-16 17:09:50 +0300

    2. Сервис - Кинотеатр

---
+++ src/main/java/ru/job4j/cinema/model/Ticket.java
@@ -0,0 +1,52 @@
+package ru.job4j.cinema.model;
+
+import ru.job4j.cinema.model.Account;
+
+public class Ticket {
+
+    private int id;
+
+    private final int sessionId;
+
+    private final int row;
+
+    private final int cell;
+
+    private final Account account;
+
+    public Ticket(int sessionId, int row, int cell) {
+        this(sessionId, row, cell,
+                new Account("", "", ""));
+    }
+
+    public Ticket(int sessionId, int row, int cell, Account account) {
+        this.sessionId = sessionId;
+        this.row = row;
+        this.cell = cell;
+        this.account = account;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSessionId() {
+        return sessionId;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCell() {
+        return cell;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
